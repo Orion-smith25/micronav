a46d27a9ff199c13e519706e8918352ed9b9d5f9
Onyx <onyx@vault.dev>
2026-08-05 08:31:27 +0800

    Bump to v0.1.1

--- waynav/__init__.py
+++ waynav/__init__.py
@@ -4,6 +4,6 @@ from .waypoint import WaypointNavigator, haversine, bearing
 from .pid import PIDController
 from .geofence import Geofence
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Smith (Maritime Autonomy Lab)"
 __all__ = ["WaypointNavigator", "haversine", "bearing", "PIDController", "Geofence"]
